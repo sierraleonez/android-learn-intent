9c96b9e2969484b06ea4b157b02de5eb55b9dcd6
almerandika@refactory.id <almerandika@refactory.id>
2021-10-30 23:52:07 +0700

    init project

---
+++ src/main/java/com/example/learnintents/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.learnintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         val btnMoveActivity : Button = findViewById(R.id.btn_mv_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_mv_activity -> {
+                
+            }
+        }
+    }
+}
